86f46f56afe6f8aa21ea5c4c86dbd001e9b49c1c
Simon Alphonse <iamsimon@outlook.in>
2021-10-01 10:28:28 +0530

    Basic Send and Receive

---
+++ BasicSendAndReceive/MessageReceiver/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace MessageReceiver
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory
+            {
+                Uri = new Uri("amqp://guest:guest@localhost:5672")
+            };
+
+            using (IConnection connection = factory.CreateConnection())
+            {
+                using (IModel channel = connection.CreateModel())
+                {
+                    string queueName = "simon-queue";
+
+                    channel.QueueDeclare(
+                        queue: queueName,
+                        durable: true,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null);
+
+                    EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (sender, e) =>
+                    {
+                        byte[] messageBytes = e.Body.ToArray();
+                        string message = Encoding.UTF8.GetString(messageBytes);
+                        Console.WriteLine($"Message received at {DateTime.Now:MMM-dd hh:mm:ss.ff} - {message}");
+                    };
+
+                    channel.BasicConsume(
+                        queueName,
+                        autoAck: true,
+                        consumer);
+
+                    Console.WriteLine($"Receiver is ready...{Environment.NewLine}");
+                    Console.ReadKey();
+                }
+            }
+
+        }
+    }
+}
---
+++ BasicSendAndReceive/MessageSender/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+
+namespace MessageSender
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                var factory = new ConnectionFactory
+                {
+                    Uri = new Uri("amqp://guest:guest@localhost:5672")
+                };
+
+                using (IConnection connection = factory.CreateConnection())
+                {
+                    using (IModel channel = connection.CreateModel())
+                    {
+                        string queueName = "simon-queue";
+
+                        channel.QueueDeclare(
+                            queue: queueName,
+                            durable: true,
+                            exclusive: false,
+                            autoDelete: false,
+                            arguments: null);
+
+                        var message = new { Name = "Simon", Message = $"Message created at {DateTime.Now:MMM-dd hh:mm:ss.ff}" };
+                        byte[] messageBytes = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
+                        Console.WriteLine(message.Message);
+                        channel.BasicPublish(string.Empty, queueName, null, messageBytes);
+                    }
+                }
+            }
+            while (Console.ReadKey().Key == ConsoleKey.Enter);
+        }
+    }
+}
